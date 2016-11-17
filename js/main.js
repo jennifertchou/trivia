00b802cb95d594789e8a1dadbf5e19f3180bb3c3
jennifertchou <Jennifer@Jennifers-MacBook-Pro-3.local>
2016-11-17 16:14:49 -0500

    keyboard commands

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
+var prevStack = []; // Stores previous QAs
+var nextStack = []; // Stores QAs that we need to restore
+
 var app = angular.module('triviaApp', ['ngAnimate']);
 app.controller('triviaCtrl', function($scope, $http, $timeout) {
   $http.get('QA.json').then(function(response){
     $scope.triviaData = response.data;
-    $scope.showRandomQA();
+    $scope.updateQA($scope.getRandomQA());
   });
 
-  $scope.showRandomQA = function() {
-    var index = $scope.getRandomIndex($scope.triviaData.length);
-    var QA = $scope.triviaData[index];
-    console.log(QA);
+  $scope.updateQA = function(QA) {
+    $scope.QA = QA;
     $scope.currentQuestion = QA["question"];
-    $scope.currentAnswer = QA["answer"];  
+    $scope.currentAnswer = QA["answer"]; 
+    $scope.startFadeOut = false;
+    $scope.answerShown = false;
   }
 
-  $scope.getRandomIndex = function(length) {
-    return Math.floor(Math.random() * length);
+  $scope.showPrevQuestion = function() {
+    if (prevStack.length) {
+      // Push current question/answer to nextStack to restore later
+      nextStack.push($scope.QA);
+      // Pop previous question/answer from prevStack and display it
+      $scope.updateQA(prevStack.pop());
+    }
   }
 
   $scope.showAnswer = function() {
@@ -27,15 +35,31 @@ app.controller('triviaCtrl', function($scope, $http, $timeout) {
   }
 
   $scope.showNextQuestion = function() {
-    console.log("show next question?");
-    if ($scope.answerShown) {
-      console.log("showing");
-      //$timeout(function(){
-        $scope.showRandomQA();
-        $scope.startFadeOut = false;
-        $scope.answerShown = false;
-      //}, 2000);
+    // Push current question/answer onto prevStack
+    prevStack.push($scope.QA);
+    // Check to see if there's stuff in nextStack to restore
+    if (nextStack.length) {
+      // Pop question off nextStack
+      $scope.updateQA(nextStack.pop());
+    } else {
+      $scope.updateQA($scope.getRandomQA());
     }
+  }
+
+  $scope.getRandomQA = function() {
+    var index = Math.floor(Math.random() * $scope.triviaData.length);
+    return $scope.triviaData[index];
+  }
+
+  $scope.key = function($event){
+      /* left 37, up 38, right 39, down 40*/
+      if ($event.keyCode == 40 || $event.keyCode == 38) { // down/up
+          $scope.showAnswer();
+      } else if ($event.keyCode == 39) { // right
+          $scope.showNextQuestion();    
+      } else if ($event.keyCode == 37) { //left
+          $scope.showPrevQuestion();
+      }
   }
 
 });
